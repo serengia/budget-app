370418df10ffba70c2b9f6ee43a8185daf726056
James Serengia <jamesserengia@gmail.com>
2023-08-15 16:39:15 +0300

    Update devise files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Validations
+  validates :name, presence: true
+  validates :email, presence: true
+
+  # Associations
+  has_many :categories, foreign_key: :author_id, dependent: :destroy
+  has_many :expenses, foreign_key: :author_id, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :category_expenses
   resources :categories
   resources :expenses
